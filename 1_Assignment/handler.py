76f81ea4efef35f88724666906b1708f031bdb3f
Tim Klages <tim.klages@fh-potsdam.de>
2022-04-16 23:39:53 +0200

    16042022

---
+++ 1_Assignment/handler.py
@@ -0,0 +1,27 @@
+# import libs
+import xml.etree.ElementTree as ET
+import pandas as pd
+import altair as alt
+
+# load and check xml
+# export_xml = file.read()
+# file.close()
+# export_xml
+
+# create etree
+tree = ET.parse('Export.xml')
+# make dictionar
+root = tree.getroot()
+record_list = [x.attrib for x in root.iter('Record')]
+# create pandas dataframe from record_list
+data = pd.DataFrame(record_list)
+# make dates from xml readable
+for col in ['creationDate', 'startDate', 'endDate']:
+    data[col] = pd.to_datetime(data[col])
+# convert "value" in xml to numeric
+data['value'] = pd.to_numeric(data['value'], errors='ignore')
+
+# check DataFrame
+data
+# show all "type" fields for selection
+data.type.unique()
